f9664ef8cd64c8112808e8149e319eb7935b3872
E S Hoogland <hoogle.e@gmail.com>
2025-07-25 21:05:31 -0400

    news-app

---
+++ NucampFolder/WebDev/2-Javascript/news-app/scripts.js
@@ -0,0 +1,17 @@
+const apiKey = process.env.NEWS_API_KEY;
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+async function fetchNews() {
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        } catch (error) {
+            console.error('Error fetching news:', error);
+        }
+}
+
+fetchNews();
