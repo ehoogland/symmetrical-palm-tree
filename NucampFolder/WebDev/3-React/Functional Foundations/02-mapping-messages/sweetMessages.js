b2f8ef8b5039f31d94770798201207ecea4bcc27
E S Hoogland <hoogle.e@gmail.com>
2025-07-27 23:03:01 -0400

    3-react sweetMessages.js

---
+++ NucampFolder/WebDev/3-React/Functional Foundations/02-mapping-messages/sweetMessages.js
@@ -0,0 +1,22 @@
+const messages = [
+    'how are you doing?',
+    'what are you up to?',
+    'would you like to get a bite later',
+
+];
+
+//const sweetMessages = `${messages[0]}, sweetie?`
+//const sweetMessages = messages.map(message => `${message}, sweetie?`);
+
+const sweetMessages = [];
+
+for (let i = 0; i < messages.length; i++) {
+    console.log(`${messages[i]}, sweetie?`);
+    sweetMessages.push(newMessage);
+}
+console
+// const sweetMessages =  [
+//     'how are you doing?', sweetie?,
+//     'what are you up to?', sweetie?,
+//     'would you like to get a bite later, sweetie?',
+// ];
