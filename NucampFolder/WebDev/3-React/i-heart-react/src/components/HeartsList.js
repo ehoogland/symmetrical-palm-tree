b571998c16807ae6a7ae1f8dc098fbf85a5d9d8c
E S Hoogland <hoogle.e@gmail.com>
2025-07-31 11:11:09 -0400

    nucampsite initial commit

---
+++ NucampFolder/WebDev/3-React/i-heart-react/src/components/HeartsList.js
@@ -0,0 +1,47 @@
+// import Heart from '../Heart';
+import CandyColoredHeart from './CandyColoredHeart'
+
+const messages = [
+    'cool cud',
+    'me my <3',
+    'you are bear',
+    'team bear',
+    'time hug',
+    'fang',
+    'bog love',
+    'me have love',
+    'all hover',
+    'sweat poo',
+    'u hack',
+    'stank love',
+    'heart me',
+    'wink bear',
+    'bear bear',
+    'be my bear',
+    'yank o way',
+    'mage love',
+    'oy',
+    'in a fan'
+];
+
+// The HeartsList component is a functional component that maps through an array of messages
+// and renders a Heart component for each message.
+// It returns a div with a className of 'hearts-container'.
+// Inside this div, it maps through the messages array and
+// renders a Heart component for each message.
+// Each Heart component receives a message as a prop;
+// the message is displayed inside the heart image
+// We are passing a callback function to the map method that takes
+// each message and returns a Heart component, with the message as a prop.
+
+const HeartsList = () => {
+    return (
+      <div className='hearts-container'>
+        {messages.map((message, idx) => (
+          <CandyColoredHeart key={idx} msg={message} />
+        ))}
+              </div>
+            );
+};
+
+export default HeartsList;
