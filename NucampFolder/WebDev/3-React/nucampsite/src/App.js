4d4423f1775fbd2342f60386f26c6fd1c4b70ba5
E S Hoogland <hoogle.e@gmail.com>
2025-08-01 00:08:59 -0400

    Week 1 workshop

--- NucampFolder/WebDev/3-React/nucampsite/src/App.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/App.js
@@ -1,25 +1,23 @@
+import Header from './components/Header';
 import CampsitesList from 'features/campsites/CampsitesList';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
+import Footer from './components/Footer';
+
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Navbar dark color='primary' sticky='top' expand='md'>
-               <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            <main>
-                <CampsitesList /> { /* The CampsitesList component will render the list of campsites. 
-                It takes care of mapping over the CAMPSITES array and displaying each campsite in a card format. That is why
-                it is used here instead of manually creating each CampsiteCard. Previously, we had to do that for each campsite
-                using the CampsiteCard component directly. Recall <CampsiteCard campsite={CAMPSITES[0]} /> and importing
-                the CampsiteCard component at the top, which is no longer necessary. It was replaced by the CampsitesList component. */ }
-            </main>
+      <Header />
+      <main>
+        <CampsitesList />
+        { /* The CampsitesList component renders the list of campsites.
+            It takes care of mapping over the CAMPSITES array and displaying each campsite in a card format. That is why
+            it is used here instead of manually creating each CampsiteCard. Previously, we had to do that for each campsite
+            using the CampsiteCard component directly. Recall starting with <CampsiteCard campsite={CAMPSITES[0]} /> and importing
+            the CampsiteCard component at the top, which is no longer necessary. It was replaced by the CampsitesList component. */ }
+      </main>
+      <Footer />
     </div>
   );
 }
