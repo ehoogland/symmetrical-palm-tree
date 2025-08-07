8ab3537305d119710360f7baebb04bad460eb068
E S Hoogland <hoogle.e@gmail.com>
2025-08-07 11:26:35 -0600

    added explicit tag props to prevent React from relying on defaultProps internally

--- NucampFolder/WebDev/3-React/nucampsite/src/features/campsites/CampsiteCard.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/features/campsites/CampsiteCard.js
@@ -2,10 +2,10 @@ import  { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 const CampsiteCard = ( {campsite} ) => {
     const { image, name } = campsite;
     return (
-        <Card>
-            <CardImg width='100%' src= {image} alt= {name} />
-            <CardImgOverlay>
-                <CardTitle>{name}</CardTitle>
+        <Card tag="div">
+            <CardImg width='100%' src= {image} alt= {name} tag="img" />
+            <CardImgOverlay tag="div">
+                <CardTitle tag='h4'>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
     );
