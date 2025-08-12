ab3cee80c9872c45f07e2a056eead8f669760f00
E S Hoogland <hoogle.e@gmail.com>
2025-08-12 15:14:30 -0400

    commit class-vs-fn and nucampsite

--- NucampFolder/WebDev/3-React/nucampsite/src/pages/CampsiteDetailPage.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/pages/CampsiteDetailPage.js
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 import { selectCampsitesById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CommentsList from '../features/Comments/CommentsList';
+import SubHeader from '../components/SubHeader';
+// CampsiteDetailPage component displays the details of a specific campsite
+// It uses the campsiteId from the URL to fetch the campsite details and comments
 const CampsiteDetailPage = () => {
     // Get the campsiteId from the URL parameters
     const { campsiteId } = useParams(); 
@@ -13,7 +16,8 @@ const CampsiteDetailPage = () => {
     const campsite = selectCampsitesById(campsiteId); 
 
     return (
-        <Container tag="div">
+        <Container>
+            <SubHeader current={campsite.name} detail={true}/>
             <Row tag="div">
                 <Col md='6' tag="div">
                     <CampsiteDetail campsite={campsite} />
