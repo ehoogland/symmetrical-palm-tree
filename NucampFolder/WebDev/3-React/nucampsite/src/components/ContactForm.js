7aac2192fdb382223e44501d7ffac90f2ca8f430
E S Hoogland <hoogle.e@gmail.com>
2025-08-20 22:14:03 -0400

    ContactForm Part 3

--- NucampFolder/WebDev/3-React/nucampsite/src/components/ContactForm.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/components/ContactForm.js
@@ -1,95 +1,121 @@
 import { Button, Label, Col, FormGroup } from 'reactstrap';
 import { Formik, Field, Form } from 'formik';
-
-/** 
- * Prop initialValues expects a JavaScript object and is used to set the initial form values. 
- * It is defined as an object literal by enclosing the key-value pairs in curly braces.
- * The outer set of curly braces is required for using JavaScript inside JSX. 
- * The inner set of curly braces is used to define a JavaScript object. 
- * @return {JSX.Element}
+import { useState } from 'react';
+/**
+ * JavaScript object that looks a lot like the initial values object, with the same properties, but
+ * the properties will be whatever the user has typed into the form fields and submitted.
+ * 
+ * The handleSubmit function is passed to Formik as a prop and is called when the form is submitted.
+ *
+ *  We will log the form values to the console. Normally we would be sending the value
+ *  to a backend databasefor processing and storage.
+ *
+ *  Also log the form values in JSON format. If you want to send the content of JavaScript objects
+ *  to another computer, they can't be sent as-is when, but can be easily converted to a
+ *  JSON text string using the JSON.stringify() method. 
+ * 
+ *  resetForm() resets the form to its initial state after form contents have been submitted
  */
+
 const ContactForm = () => {
-return (
-    <Formik
-      initialValues={{ 
-        firstname: '', 
-        lastName: '', 
-        phoneNumber: '', 
-        email: '', 
-        agree: false, 
-        contactType: 'By Phone', 
-        feedback: '' 
-      }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      { /* Form is imported from Formik and is used to create a form with validation and submission handling. */ }
-      { /* FormGroup is from reactstrap and is used to group form controls and labels together in a structured way.
-      Similar to bootstrap's form-group */ }
-      <Form>
-        <FormGroup row>
-            <Label htmlFor='firstName' md='2'>
-                First Name
-            </Label>
-            <Col md='10'>
-                <Field name='firstName' placeholder='First Name' className='form-control' />
-            </Col>
-        </FormGroup>
-        <FormGroup row>
-            <Label htmlFor='lastName' md='2'>
-                Last Name
-            </Label>
-            <Col md='10'>
-                <Field name='lastName' placeholder='Last Name' className='form-control' />
-            </Col>
-        </FormGroup>
-        <FormGroup row>
-            <Label htmlFor='phoneNum' md='2'>
-                Phone
-            </Label>
-            <Col md='10'>
-                <Field name='phoneNum' placeholder='Phone' className='form-control' />
-            </Col>
-        </FormGroup>
-        <FormGroup row>
-            <Label htmlFor='email' md='2'>
-                Email
-            </Label>
-            <Col md='10'>
-                <Field name='email' placeholder='Email' className='form-control' />
-            </Col>
-        </FormGroup>
-        <FormGroup row>
-            <Label check md={{ size: 4, offset: 2 }}>
-                <Field
-                    name='agree'
-                    type='checkbox'
-                    className='form-check-input'
-                />{' '}
-                May we contact you?
-            </Label>
-            <Col md='4'>
-            {/* A select menu for contact type instead of a form field*/}
-                <Field name='contactType' as='select' className='form-control'>
-                    <option>By Phone</option>
-                    <option>By Email</option>
-                </Field>
-            </Col>
-        </FormGroup>
-        <FormGroup row>
-            <Label htmlFor='feedback' md='2'>
-                Your Feedback
-            </Label>
-            <Col md='10'>
-                <Field name='feedback' as='textarea' rows='12' className='form-control' />
-            </Col>
-        </FormGroup>
-        <FormGroup row>
-        </FormGroup>
-      </Form>
-    </Formik>
-  );
+    const [formKey, setFormKey] = useState(0);
+    const handleSubmit = (values, { resetForm }) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+        setFormKey(prev => prev + 1);
+    };
+    /** 
+     * Prop initialValues expects a JavaScript object and is used to set the initial form values. 
+     * It is defined as an object literal by enclosing the key-value pairs in curly braces.
+     * The outer set of curly braces is required for using JavaScript inside JSX. 
+     * The inner set of curly braces is used to define a JavaScript object. 
+     *
+     * A Key prop is added to the Formik component that is set to a state variable formKey. This was
+     * done to ensure the form field cleared, as it was failing to clear firstName for some reason.
+     * @return {JSX.Element}
+     */
+    return (
+        <Formik
+            key={formKey}
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                phoneNum: '', 
+                email: '', 
+                agree: false, 
+                contactType: 'By Phone', 
+                feedback: ''
+            }}
+            onSubmit={handleSubmit}
+        >
+            <Form>
+                <FormGroup row>
+                    <Label htmlFor='firstName' md='2'>
+                        First Name
+                    </Label>
+                    <Col md='10'>
+                        <Field name='firstName' placeholder='First Name' className='form-control' />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='lastName' md='2'>
+                        Last Name
+                    </Label>
+                    <Col md='10'>
+                        <Field name='lastName' placeholder='Last Name' className='form-control' />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='phoneNum' md='2'>
+                        Phone
+                    </Label>
+                    <Col md='10'>
+                        <Field name='phoneNum' placeholder='Phone' className='form-control' />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='email' md='2'>
+                        Email
+                    </Label>
+                    <Col md='10'>
+                        <Field name='email' placeholder='Email' className='form-control' />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label check md={{ size: 4, offset: 2 }}>
+                        <Field
+                            name='agree'
+                            type='checkbox'
+                            className='form-check-input'
+                        />{' '}
+                        May we contact you?
+                    </Label>
+                    <Col md='4'>
+                        <Field name='contactType' as='select' className='form-control'>
+                            <option>By Phone</option>
+                            <option>By Email</option>
+                        </Field>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='feedback' md='2'>
+                        Your Feedback
+                    </Label>
+                    <Col md='10'>
+                        <Field name='feedback' as='textarea' rows='12' className='form-control' />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Col md={{ size: 10, offset: 2 }}>
+                        <Button type='submit' color='primary'>
+                            Send Feedback
+                        </Button>
+                    </Col>
+                </FormGroup>
+            </Form>
+        </Formik>
+    );
 };
 
 export default ContactForm;
