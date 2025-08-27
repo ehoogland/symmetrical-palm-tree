3f906de61e343cac969f69e838dbc37f0ffbdae5
E S Hoogland <hoogle.e@gmail.com>
2025-08-27 01:39:06 -0400

    Redux campsitesSlice

--- NucampFolder/WebDev/3-React/nucampsite/src/app/store.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/app/store.js
@@ -1,8 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { campsitesReducer } from '../features/campsites/campsitesSlice';
+/**
+ * The counter feature, including the counterSlice reducer were included
+ * in the Redux template as example code, and were replaced by
+ * campsiteReducer.
+ 
+import counterReducer from '../features/counter/counterSlice'; */
 
+/** 
+ * You can think of the store as a slightly more complicated example
+ * of the useReducer hook
+ *
+ * The call to the configureStore function is where the Redux store is created.
+ * It takes a configuration object as an argument.
+ * The configuration object typically includes a reducer property, a function that 
+ * determines how the state is updated in response to actions.
+ * Each time we add a property to the reducer object, it will create a corresponding
+ * property in our state object that is maintained by the corresponding reducer.
+ * 
+ * The configureStore function automatically sets up the Redux DevTools extension
+ * and includes some useful middleware like loggerby default.
+ * Replace counterReducer example code with campsiteReducer.
+ * Add it to the store by passing it into the configureStore
+ * function as a property of the reducer object */
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    //counter: counterReducer,
+    campsites: campsitesReducer,
   },
 });
--- NucampFolder/WebDev/3-React/nucampsite/src/index.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/index.js
@@ -13,6 +13,17 @@ import 'typeface-open-sans';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+/**
+ * Pass the [Redux] store to the App component
+ * The store is imported into this file and
+ * passed to the Provider component as a prop, which then makes it available
+ * to the App component and all its child components. The store is an object 
+ * that acts as a container around the state object that contains the global 
+ * application state. It also has useful methods such as dispatch, which is 
+ * typically accessed via the useDispatch hook.
+ */
+console.log(store.dispatch);
+console.log(store.getState());
 
 root.render(
   <React.StrictMode>
