f5867e9b439659f4c71d21e3fb53a9a1c80e74cd
E S Hoogland <hoogle.e@gmail.com>
2025-09-05 16:00:21 -0400

    portfolio project

--- portfolio-project/src/components/RecipeSearch.js
+++ portfolio-project/src/components/RecipeSearch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import IngredientCard from '../IngredientCard';
 import { recipeService } from '../services/recipeService';
 import { Link } from 'react-router-dom';
