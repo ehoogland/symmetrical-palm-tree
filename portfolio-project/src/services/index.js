47bcad4c5c497a19e42f49b079dc3b207b29f20f
E S Hoogland <hoogle.e@gmail.com>
2025-09-06 11:17:30 -0400

    chore(services): point index to renamed recipe.js

--- portfolio-project/src/services/index.js
+++ portfolio-project/src/services/index.js
@@ -1,5 +1,5 @@
 // Service entrypoint - export the real recipeService implementation.
-import { recipeService } from './recipeService';
+import { recipeService } from './recipe';
 
 export { recipeService };
 export default recipeService;
