3e498839c9517e24641e599e27be701756808833
E S Hoogland <hoogle.e@gmail.com>
2025-09-19 12:47:45 -0400

    Getting Started with React Native

--- NucampFolder/WebDev/4-React-Native.nosync/nucampsite/App.js
+++ NucampFolder/WebDev/4-React-Native.nosync/nucampsite/App.js
@@ -1,12 +1,21 @@
-//import { StatusBar } from 'expo-status-bar';
-//import { StyleSheet, Text, View } from 'react-native';
-// replace with a single import statement from the MainComponent.js file
-import Main from './screens/MainComponent';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
