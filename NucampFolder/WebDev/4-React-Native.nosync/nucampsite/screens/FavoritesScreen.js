c218b787290bee83e504070db8a9d90321725e17
E S Hoogland <hoogle.e@gmail.com>
2025-10-01 12:37:43 -0400

    Alert

--- NucampFolder/WebDev/4-React-Native.nosync/nucampsite/screens/FavoritesScreen.js
+++ NucampFolder/WebDev/4-React-Native.nosync/nucampsite/screens/FavoritesScreen.js
@@ -1,8 +1,11 @@
-import { useSelector } from 'react-redux';
-import { View, FlatList, Text } from 'react-native';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { View, FlatList, Text, TouchableOpacity, Alert, StyleSheet } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import Loading from '../components/LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { SwipeRow } from 'react-native-swipe-list-view';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 /**
  * 
  * @description The FavoritesScreen component displays a list of favorite campsites.
@@ -25,57 +28,115 @@ import { baseUrl } from '../shared/baseUrl';
  * @prop {JSX.Element} title - The title to display in the list item. In this case, it is
  * a Text component displaying the name of the campsite.
  * @returns {JSX.Element} The FavoritesScreen component rendering a list of favorite campsites.
+ * @function renderFavoriteItem - A helper function to render each favorite campsite item.
+ * @param {Object} item - The campsite item to be rendered, destructured to extract the campsite property.
+ * @param {Object} campsite - The campsite object
+ * @property {string} campsite.name - The name of the campsite.
+ * @property {string} campsite.image - The image source of the campsite.
+ * @property {string} campsite.description - The description of the campsite.
+ * @function onPress - arrow function [declaration] to handle press events on the ListItem component.
+ * @prop {Object} navigation - The navigation prop provided by React Navigation.
+ * @function navigate - React Navigation function to navigate to different screens.
+ * In this case, it navigates to the CampsiteInfoScreen within the DirectoryNav stack,
+ * passing the selected campsite as a parameter.
+ * 
+ * Lesson code had navigation.popTo(...) function, but that errors as not being a function. 
+ * Changed to navigation.navigate. 
+ * "CampsiteInfo" file referred to in code does not exist. I recalled "CampsiteInfoScreen" in screens
+ * subdirectory, and it is listed in MainComponent.js as "CampsiteInfoScreen", so I changed to that.
+ * 
+ * @note The navigate function is used here to navigate to a nested screen (CampsiteInfoScreen)
+ * within a nested navigator (DirectoryNav). This is done by specifying the screen name and
+ * passing the necessary parameters.
+ * @note The FavoritesScreen component assumes that it is part of a navigation stack
+ * and that the navigation prop is passed to it. This is typically done when the component
+ * is used as a screen in a navigator provided by React Navigation.
+ * 
  */
 const FavoritesScreen = ({ navigation }) => {
+    const dispatch = useDispatch();
     const favorites = useSelector((state) => state.favorites);
     const { campsitesArray, isLoading, errorMessage } = useSelector((state) => state.campsites);
-    const renderFavoriteItem = ({ item: campsite }) => {
-        return (
-            <ListItem
-                onPress={() =>
-                    navigation.navigate('DirectoryNav', {
-                        screen: 'CampsiteInfoScreen',
-                        params: { campsite }
-                    })
-                }
-            >
-                <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
+
+    const confirmDelete = (campsite) => {
+        Alert.alert(
+            'Delete Favorite?',
+            `Are you sure you wish to delete the favorite campsite ${campsite.name}?`,
+            [
+                { text: 'Cancel', onPress: () => console.log(campsite.name + ' Not Deleted'), style: 'cancel' },
+                { text: 'OK', onPress: () => dispatch(toggleFavorite(campsite.id)) }
+            ],
+            { cancelable: false }
         );
     };
 
+    const renderFavoriteItem = ({ item: campsite }) => (
+        <View>
+            <SwipeRow rightOpenValue={-100} disableRightSwipe>
+                <View style={styles.deleteView}>
+                    <TouchableOpacity style={styles.deleteTouchable} onPress={() => confirmDelete(campsite)}>
+                        <Text style={styles.deleteText}>Delete</Text>
+                    </TouchableOpacity>
+                </View>
 
-    if (isLoading) {
-        return <Loading />;
-    }
+                <View>
+                    <ListItem
+                        onPress={() =>
+                            navigation.navigate('DirectoryNav', {
+                                screen: 'CampsiteInfoScreen',
+                                params: { campsite }
+                            })
+                        }
+                    >
+                        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+                        <ListItem.Content>
+                            <ListItem.Title>{campsite.name}</ListItem.Title>
+                            <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                </View>
+            </SwipeRow>
+        </View>
+    );
 
-    if (errorMessage) {
-        return ( 
+    if (isLoading) return <Loading />;
+
+    if (errorMessage) return (
         <View>
             <Text>{errorMessage}</Text>
         </View>
-        );
-    }
+    );
 
     return (
         <FlatList
-            data={campsitesArray.filter((campsite) =>
-                favorites.includes(campsite.id)
-            )}
+            data={campsitesArray.filter((campsite) => favorites.includes(campsite.id))}
             renderItem={renderFavoriteItem}
             keyExtractor={(item) => item.id.toString()}
         />
     );
 };
 
-// removed duplicate renderFavoriteItem helper
-          
-
-
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'center',
+        paddingHorizontal: 20
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+});
 
 
 
