ede110dc7b400db5d7546ffe965ee73ed62136c6
E S Hoogland <hoogle.e@gmail.com>
2025-10-06 17:23:34 -0400

    Sending Email

--- NucampFolder/WebDev/4-React-Native.nosync/nucampsite/screens/ContactScreen.js
+++ NucampFolder/WebDev/4-React-Native.nosync/nucampsite/screens/ContactScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import {Card, Text} from 'react-native-elements';   
+// Add Button and Icon; React Native also has a Button component but it has a configuration option
+// to set an icon to be used on the button
+import {Card, Text, Button, Icon} from 'react-native-elements'; 
+// MailComposer API to compose and send emails from within the app
+import * as MailComposer from 'expo-mail-composer'; 
 
 const ContactScreen = () => {
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      recipients: ['campsites@nucamp.co'],
+      subject: 'Inquiry',
+      body: 'To whom it may concern:'
+    });
+    };
+
     return (
       <ScrollView>
         <Animatable.View
@@ -12,13 +24,26 @@ const ContactScreen = () => {
             delay={1000}
         >
           <Card wrapperStyle={{margin: 20}}>
+
             <Card.Title>Contact Information</Card.Title>
               <Card.Divider/>
               <Text>1 Nucamp Way</Text>
-            <Text>Seattle, WA 98001</Text>
-            <Text style={{marginBottom: 5}}>U.S.A.</Text>
+              <Text>Seattle, WA 98001</Text>
+              <Text style={{marginBottom: 5}}>U.S.A.</Text>
               <Text>Phone: 1-206-555-1234</Text>
               <Text>Email: campsites@nucamp.co</Text>
+              <Button
+                  title='Send Email'
+                  buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                  icon={<Icon 
+                    name='envelope-o' // outlined version of the envelope icon in FontAwesome
+                    type='font-awesome' 
+                    color='white' 
+                    iconStyle={{marginRight: 10}} // space between icon and title
+                  /> // closing tag for Icon self-closing component
+                  } // closing tage for icon prop
+                  onPress = { () => {sendMail() } } // added arrow function to call sendMail function on press
+              /> {/* closing tag for Button self-closing component */}
           </Card>
         </Animatable.View>
       </ScrollView>
