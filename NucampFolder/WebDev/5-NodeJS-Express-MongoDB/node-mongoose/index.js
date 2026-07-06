65a250ef899edb30dd984e5402df311ba55e1804
E S Hoogland <hoogle.e@gmail.com>
2026-07-06 01:03:18 -0400

    Mongoose Part 3

--- NucampFolder/WebDev/5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ NucampFolder/WebDev/5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -1,55 +1,129 @@
+// index.js
+// Import the mongoose module, which is an Object Data Modeling (ODM) library for MongoDB and Node.js
+// It provides a schema-based solution to model application data,
+// and includes built-in type casting, validation, query building, business logic hooks and more
+// The mongoose module is used to connect to a MongoDB database, define schemas and models,
+// and perform CRUD operations on the documents in the database
 const mongoose = require('mongoose');
+// Import the Campsite model, which is defined in the models/campsite.js file
+// The Campsite model is used to interact with the campsites collection in the database
+// The Campsite model is created by calling mongoose.model() and passing in the name of the model and the schema
+// The model name is the singular name of the collection, and Mongoose will automatically look for the plural, 
+// lowercase version of the model name in the database
 const Campsite = require('./models/campsite');
 
 const url = 'mongodb://127.0.0.1:27017/nucampsite';
+// The connect variable is a promise that resolves when the connection to the database is successful
 const connect = mongoose.connect(url, {});
-
+// If the connection is successful, the promise resolves and the .then() method is called
+// If the connection fails, the promise is rejected and the .catch() method is called
 connect.then(() => {
-
+    
     console.log('Connected correctly to server');
-
+    // create is a method of the Campsite model that creates a new document in the campsites collection
+    // The create method takes an object that defines the fields and values of the new document
+    // The create method returns a promise that resolves to the newly created document
+    // .then() is used to handle the promise returned by Campsite.create()
+    //
     Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
     })
-    // .then() is used to handle the promise returned by Campsite.create()
+    /**
+     * Constructor functions are used to create new instances of a model, which are called documents.
+     * The new keyword is used to create a new instance of the Campsite model, which is a document in the campsites collection.
+     * The new Campsite() constructor takes an object that defines the fields and values of the new document.
+     * The new Campsite() constructor does not save the document to the database, it only creates a new instance of the model in memory.
+     * The save() method is called on the new instance of the model to save the document to the database.
+     * The save() method returns a promise that resolves to the newly created document.
+     * 
+     * Example:  
+     * const newCampsite = new Campsite({
+     * name: 'React Lake Campground',
+     * description: 'test'
+     * });  
+     * newCampsite.save() -- Now there is no need to use the save() method here because the create() method already saves the document to the database, unlike
+     * the new Campsite() constructor, which creates a new document in memory but does not save it to the database.
+     */
+
     // The first .then() is called when the promise is resolved successfully
-    // The second .then() is called when the promise returned by Campsite.findByIdAndUpdate() is resolved successfully
-    // The third .then() is called when the promise returned by campsite.save() is resolved successfully
-    // The fourth .then() is called when the promise returned by Campsite.deleteMany() is resolved successfully
-    // The .catch() is called when any of the promises are rejected
-    //
     .then(campsite => {
         console.log(campsite);
-
+        // The findByIdAndUpdate() method is used to find a document by its _id field and update it with new values
+        // The findByIdAndUpdate() method takes three arguments:
+        // 1. The _id of the document to find
+        // 2. An object that defines the fields and values to update
+        // 3. An options object that can be used to customize the behavior of the method
+        // The new option is set to true, which means that the updated document will be returned
+        // If new is set to false, the original document will be returned
+        // The findByIdAndUpdate() method returns a promise that resolves to the updated document
+        // This replaces the find() and save() methods that were used in previous versions of Mongoose to update a document
+        // The findByIdAndUpdate() method is a shorthand for the findOneAndUpdate() method, which is used to find a document 
+        // by a specific field and update it with new values
         return Campsite.findByIdAndUpdate(campsite._id, {
             /* $set is used to update the value of a field 
             without affecting other fields in the document */
             $set: { description: 'Updated Test Document' }
         }, {
+            // The new option is set to true, which means that the updated document will be returned
+            // If new is set to false, the original document will be returned
             new: true
         });
     })
+    // The second .then() is called when the promise returned by Campsite.findByIdAndUpdate() is resolved successfully
+    // The campsite variable in this .then() is the updated document returned by findByIdAndUpdate()
+    // The findByIdAndUpdate() method is used to find a document by its _id field and update it with new values
+    // The findByIdAndUpdate() method takes three arguments:
+    // 1. The _id of the document to find
+    // 2. An object that defines the fields and values to update
+    // 3. An options object that can be used to customize the behavior of the method
+    // The new option is set to true, which means that the updated document will be returned
+    // If new is set to false, the original document will be returned
+    // The findByIdAndUpdate() method returns a promise that resolves to the updated document
+    // The campsite variable in this .then() is the updated document returned by findByIdAndUpdate()
     .then(campsite => {
         console.log(campsite);
-
+        
+        // An array of comments is added to the campsite document using the push() method
+        // The push() method adds a new comment object to the comments array in the campsite document
+        // The comment object includes fields for rating, text, and author
+        // The comment object is defined inline, but it could also be defined as a separate variable
         campsite.comments.push({
             rating: 5,
             text: 'What a magnificent view!',
             author: 'Tinus Lorvaldes'
         });
-
+        // .save() is a method called on a document that saves the changes made to a [sub]document in the database
+        // The campsite.save() method is called to save the changes made to the campsite document in the database
+        // It returns a promise that resolves to the updated document
         return campsite.save();
     })
+    // The third .then() is called when the promise returned by campsite.save() is resolved successfully
     .then(campsite => {
         console.log(campsite);
+        // The deleteMany() method is called on the Campsite model to delete all documents in the campsites collection
+        // The deleteMany() method takes an optional filter object that can be used to specify which documents to delete
+        // If no filter object is provided, all documents in the collection will be deleted
+        // The deleteMany() method returns a promise that resolves to an object that contains information about the operation
+        // The object returned by deleteMany() includes a deletedCount field that indicates the number of documents deleted
         return Campsite.deleteMany();
     })
+    // The fourth .then() is called when the promise returned by Campsite.deleteMany() is resolved successfully
     .then(() => {
+        // The mongoose.connection.close() method is called to close the connection to the database
+        // Closing the connection is important to free up resources and prevent memory leaks
+        // The return statement is used to return the promise returned by mongoose.connection.close()
+        // This allows the next .then() or .catch() in the promise chain to be called after the connection is closed
         return mongoose.connection.close();
     })
+    // The .catch() is called when any of the promises are rejected
     .catch(err => {
         console.log(err);
+        // The mongoose.connection.close() method is called to close the connection to the database. No return
+        // statement is needed here because this is the last statement in the promise chain.
+        // Is it necessary to save a module to a variable in order to use it? No, but it is a good practice to do so.
+        // The mongoose variable is a reference to the mongoose module, which is an object that contains all the 
+        // methods and properties of the module.
         mongoose.connection.close();
     });
 });
--- NucampFolder/WebDev/5-NodeJS-Express-MongoDB/node-mongoose/models/campsite.js
+++ NucampFolder/WebDev/5-NodeJS-Express-MongoDB/node-mongoose/models/campsite.js
@@ -1,15 +1,60 @@
+// models/campsite.js
+// Import the mongoose module, which is an Object Data Modeling (ODM) library for MongoDB and Node.js
+// It provides a schema-based solution to model application data,
+// and includes built-in type casting, validation, query building, business logic hooks and more
+// The mongoose module is used to connect to a MongoDB database, define schemas and models,
+// and perform CRUD operations on the documents in the database
 const mongoose = require('mongoose');
+// Schema is a constructor function that is used to create a new schema object
+// A schema is a blueprint for a model, which defines the structure of the documents in a collection
+// The schema defines the fields and their types, as well as any validation rules or default values
+// The schema can also define:
+// virtual properties, instance methods, and static methods;
+// virtuals, which are properties that are not stored in the database; 
+// custom virtuals, which are properties that are computed from other properties;
+// indexes, which are used to improve query performance;
+// middleware, which are functions that are executed before or after certain operations;
+// plugins, which are reusable pieces of code that can be added to a schema;
+// discriminators, which are used to create multiple models with different schemas 
+// that share the same underlying collection;
+// subdocuments, which are documents that are embedded within other documents;
+// arrays, which are used to store multiple values in a single field;
+// custom arrays, which are used to store multiple values in a single field;
+// references, which are used to create relationships between documents in different collections;
+// getters and setters, which are functions that are called when a property is accessed or modified;
+// custom types, which are user-defined types that can be used in the schema;
+// custom validators, which are functions that are used to validate the values of fields;
+// custom error messages, which are used to provide more informative error messages;
+// custom options, which are used to customize the behavior of the schema;
+// custom methods, which are functions that are defined on the schema 
+// and can be called on instances of the model;
+// custom statics, which are functions that are defined on the schema
+// and can be called on the model itself;
+// custom virtuals, which are properties that are computed from other properties or are properties 
+// that are not stored in the database;
+// custom indexes, which are used to improve query performance;
+// custom middleware, which are functions that are executed before or after certain operations;
+// custom plugins, which are reusable pieces of code that can be added to a schema;
+// custom discriminators, which are used to create multiple models with different schemas
+// that share the same underlying collection;
+// custom subdocuments, which are documents that are embedded within other documents;
+// custom references, which are used to create relationships between documents in different collections;
+// custom getters and setters, which are functions that are called when a property is accessed or modified;
+
 const Schema = mongoose.Schema;
-// Load the mongoose-currency package which includes the loadType function
-// and add it to mongoose. This allows us to use the Currency type in our schemas
-// Note: This package is not included in the default mongoose package, 
-// so we need to install it separately
-// npm install mongoose-currency
-require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency; // Currency type, 
-// which is a subclass of Number, that allows us to store currency values in our schemas
 
 // Define the comment schema, which will be used as a subdocument in the campsite schema
+// The comment schema defines the structure of the comments array in the campsite schema
+// The comment schema includes fields for rating, text, and author, as well as the timestamps option
+// The timestamps option automatically adds createdAt and updatedAt fields to the comment schema
+// The createdAt field will store the date and time when the comment was created
+// The updatedAt field will store the date and time when the comment was last updated
+// The timestamps option is a boolean that can be set to true or false
+// If set to true, Mongoose will automatically add the createdAt and updatedAt fields to the schema
+// The comment schema is used to create an array of comments in the campsite schema
+// The comments array is an array of comment subdocuments, which are defined by the comment schema
+// The comment schema is also used to validate the data in the comments array
+// The comment schema is also used to define the structure of the comments array in the campsite schema
 const commentSchema = new Schema({
     rating: {
         type: Number,
@@ -26,10 +71,21 @@ const commentSchema = new Schema({
         required: true
     }
 }, {
+    // Automatically add createdAt and updatedAt fields to the comment schema
+    // The createdAt field will store the date and time when the comment was created
+    // The updatedAt field will store the date and time when the comment was last updated
+    // The timestamps option is a boolean that can be set to true or false
+    // If set to true, Mongoose will automatically add the createdAt and updatedAt fields to the schema
     timestamps: true
 });
 
 // Define the campsite schema, which includes an array of comments
+// The campsite schema defines the structure of the documents in the campsites collection
+// The campsite schema includes fields for name, description, image, elevation, cost, featured, and comments
+// The comments field is an array of comment subdocuments, which are defined by the comment schema
+// The campsite schema also includes the timestamps option, which automatically adds createdAt and 
+// updatedAt fields to the schema
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -40,28 +96,22 @@ const campsiteSchema = new Schema({
         type: String,
         required: true
     },
-    image: {
-        type: String,
-        required: true
-    },
-    elevation: {
-        type: Number,
-        required: true
-    },
-    cost: {
-        type: Currency,
-        required: true,
-        min: 0
-    },
-    featured: {
-        type: Boolean,
-        default: false
-    },
     comments: [commentSchema] // Array of comment subdocuments
 }, {
     timestamps: true // Automatically add createdAt and updatedAt fields
 });
 
-// Export the Campsite model based on the campsite schema       
-
+// Export the Campsite model based on the campsite schema
+// The model is a constructor function that is used to create instances of the model
+// Why not just export the schema? 
+// Because the model is what we use to interact with the database. In Mongoose, a model is a wrapper for the schema 
+// that provides an interface to the database for creating, querying, updating, and deleting documents
+// The model is created by calling mongoose.model() and passing in the name of the model and the schema
+// The model name is the singular name of the collection, and Mongoose will automatically look for the plural, 
+// lowercased version of the model name in the database
+// Recall that a collection is a group of documents in the database that share the same schema
+// Schema > Model > Collection > Document. 
+// In SQL, this is similar to Database > Table > Row > Column
+// The model is used to create and query documents in the collection and to perform CRUD operations on the documents in the collection
+// For example, if the model name is 'Campsite', Mongoose will look for a collection named 'campsites' in the database.
 module.exports = mongoose.model('Campsite', campsiteSchema);
